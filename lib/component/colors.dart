5555e6b5eff5073c7cfb87ff3ce31fdc41edb21f
Yashi6633 <nishathyasintha@gmail.com>
2022-07-19 05:11:51 +0530

    Orders completed page

--- lib/component/colors.dart
+++ lib/component/colors.dart
@@ -3,5 +3,9 @@ import 'dart:ui';
 class AppColor{
   static Color mainColor = Color(0xFF94B447);
   static Color textDark = Color(0xFF9FA2B4);
+  static Color compColor = Color(0xFF58B862);
+  static Color processColor = Color(0xFFF3ED4E);
+  static Color rejectColor = Color(0xFFF25D5D);
+
 
 }
