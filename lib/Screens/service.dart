13224a880bab6f101af5891683a611da522b6ed7
Yashi6633 <nishathyasintha@gmail.com>
2022-07-23 14:49:21 +0530

    responsive service

--- lib/Screens/service.dart
+++ lib/Screens/service.dart
@@ -3,8 +3,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sprace/component/colors.dart';
 
-// import '../component/header.dart';
-import '../component/sidemenu.dart';
 
 class ServicePage extends StatefulWidget {
   const ServicePage({Key? key}) : super(key: key);
